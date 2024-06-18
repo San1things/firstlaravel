b628f1cf863462ac85821c79f329bcdbbe053f30
San1things <businesslikesan1@gmail.com>
2024-06-18 22:39:06 +0800

    first

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/faq', function () {
+    return view('faq');
+});
