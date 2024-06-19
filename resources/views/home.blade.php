e3cdb6f11dae172ec52a403195a34dd5a1341855
San1things <businesslikesan1@gmail.com>
2024-06-19 14:49:24 +0800

    inayos yung sa template shi

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,73 @@
-@extends('layout.layout')
+@extends('template.template')
+
+@section('content')
+
+<section class="header">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="/">HOME</a>
+      <button class="navbar-toggler shadow-none border-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="sidebar offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+        <div class="offcanvas-header">
+          <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Dark offcanvas</h5>
+          <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+          <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="/">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/faq">FAQ</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/about">About</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Other Shi
+              </a>
+              <ul class="dropdown-menu dropdown-menu-dark">
+                <li><a class="dropdown-item" href="#">Try</a></li>
+                <li><a class="dropdown-item" href="#">Try</a></li>
+                <li>
+                  <hr class="dropdown-divider">
+                </li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </li>
+          </ul>
+          <form class="d-flex mt-3" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-success" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </nav>
+</section>
+
+<section class="home">
+  <div class="main-wrapper flex-box">
+    <div class="add-todo">
+      <button class="btn btn-primary add-button">ADD</button>
+    </div>
+    <div class="cards-todo">
+      <div class="card" style="width: 20rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Sample</h5>
+          <p class="card-text">Gumawa ng project bago mag apply.</p>
+        </div>
+        <div class="card-body">
+          <button class="btn btn-primary">Update</button>
+          <button class="btn btn-warning">Delete</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+@endsection
