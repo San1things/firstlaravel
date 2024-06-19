0231ee2b46a68ef90af6eb9b65937895bfaa8abe
San1things <businesslikesan1@gmail.com>
2024-06-19 22:34:34 +0800

    change cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -55,15 +55,50 @@
       <button class="btn btn-primary add-button">ADD</button>
     </div>
     <div class="cards-todo">
-      <div class="card" style="width: 20rem;">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Sample</h5>
-          <p class="card-text">Gumawa ng project bago mag apply.</p>
+      <div class="row row-cols-1 row-cols-md-3 g-4">
+        <div class="col">
+          <div class="card">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title 1</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <button class="btn btn-primary card-btn">Update</button>
+              <button class="btn btn-warning card-btn">Delete</button>
+            </div>
+          </div>
         </div>
-        <div class="card-body">
-          <button class="btn btn-primary">Update</button>
-          <button class="btn btn-warning">Delete</button>
+        <div class="col">
+          <div class="card">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title 2</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <button class="btn btn-primary card-btn">Update</button>
+              <button class="btn btn-warning card-btn">Delete</button>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title 3</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+              <button class="btn btn-primary card-btn">Update</button>
+              <button class="btn btn-warning card-btn">Delete</button>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title 4</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <button class="btn btn-primary card-btn">Update</button>
+              <button class="btn btn-warning card-btn">Delete</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
